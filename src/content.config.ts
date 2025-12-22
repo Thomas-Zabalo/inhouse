c10a2df227bf1ec7c27b12810a0f0ed90f848654
Thomas <thomas.zabalo@gmail.com>
2025-12-22 20:51:37 +0100

    remove player stats mdx

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,7 +3,7 @@ import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
 const blog = defineCollection({
-  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/player" }),
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/blog" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
